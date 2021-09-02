2029713e329b31120c9ae660bafceca9040d0f1a
Andrew <rolyata47@gmail.com>
2021-09-02 12:00:47 -0500

    Committing code to hexagon.py, which draws a hexagon using the turtle module

---
+++ hexagon.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.shape('turtle')
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+
+turtle.done()
