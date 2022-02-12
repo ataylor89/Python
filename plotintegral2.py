1e16296340d646036806fd9c51beb5c5aa18ea5e
Andrew Taylor <rolyata47@gmail.com>
2022-02-12 16:50:08 -0600

    Adding matplotlib plot for y=x^2

---
+++ plotintegral2.py
@@ -0,0 +1,28 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.patches import Polygon
+
+def func(x):
+    return x**2
+
+a, b = 7, 9  # integral limits
+x = np.linspace(0, 10)
+y = func(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y, 'r', linewidth=2)
+ax.set_ylim(bottom=0)
+plt.xticks(range(11))
+plt.yticks(range(0, 110, 10))
+
+# Make the shaded region
+ix = np.linspace(a, b)
+iy = func(ix)
+verts = [(a, 0), *zip(ix, iy), (b, 0)]
+poly = Polygon(verts, facecolor='0.9', edgecolor='0.5')
+ax.add_patch(poly)
+
+ax.set_xticks([a, b], labels=['$x$', '$x+h$'])
+ax.set_yticks([func(a), func(b)], labels=['$x^2$', '$(x+h)^2$'])
+
+plt.show()
