a995625a9b8fd1e3f7960c240169756c4dd39326
Andrew Taylor <rolyata47@gmail.com>
2022-02-18 14:04:07 -0600

    Updating tkdemo2.py

--- tkdemo2.py
+++ tkdemo2.py
@@ -8,13 +8,25 @@ class Application(tk.Frame):
         self.createWidgets()
 
     def createWidgets(self):
-        rows = 5
-        cols = 5
-        for i in range(rows):
-            for j in range(cols):
-                cell = tk.Entry(self, text="")
-                cell.grid(row=i, column=j)       
+        username_label = tk.Label(self, text="Username")
+        username_label.grid(row=0, column=0)
+        email_label = tk.Label(self, text="Email")
+        email_label.grid(row=0, column=1)
+        join_date_label = tk.Label(self, text="Join date")
+        join_date_label.grid(row=0, column=2)
+        group_label = tk.Label(self, text="Group")
+        group_label.grid(row=0, column=3)
+        institution_label = tk.Label(self, text="Institution")
+        institution_label.grid(row=0, column=4)
+        for i in range(1, 10):
+            for j in range(5):
+                if j < 4:
+                    cell = tk.Entry(self, text="", state=tk.DISABLED, disabledbackground="white")
+                    cell.grid(row=i, column=j)      
+                else:
+                    cell = tk.Entry(self, text="")
+                    cell.grid(row=i, column=j)       
 
 app = Application()                       
-app.master.title('Sample application')    
+app.master.title('User Report')    
 app.mainloop()                   
