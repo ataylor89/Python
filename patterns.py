20e047000c3fbcb7b92efe636eb1ad81eed84ec0
Andrew Taylor <rolyata47@gmail.com>
2022-08-07 18:59:50 -0500

    Adding some comments and more strings

--- patterns.py
+++ patterns.py
@@ -9,7 +9,7 @@
 import re
 
 # The re.compile method gives us a pattern object. The pattern object has a method called match
-pattern = re.compile(r'How [a-zA-Z]+ly .*!')
+pattern = re.compile(r'How [a-zA-Z]+ly.*!')
 
 strings = [
     "How lovely the sky is!", 
@@ -26,6 +26,27 @@ strings = [
     "How softly he sings!"]
 
 print("Matches:")
+for s in strings:
+    if pattern.match(s):
+        print(s)
+
+# It's good to ask, why does the match method belong to the pattern object, and not the string object?
+#
+# Well, if the match method belonged to the string object, it would be loaded into memory in every program, 
+# because string is a built-in data type, and the string class is always loaded into memory.
+#
+# Thus it might save memory to put the match() method in the pattern class. This way it only gets loaded into memory when needed.
+
+strings = [
+    "How beautifully he sings!",
+    "How beautifully she sings!",
+    "How softly they whisper!",
+    "How quietly they play!",
+    "How beautifully he plays the piano!",
+    "The syllabus is on the website",
+    "The homework is due Friday",
+    "how curiously"]
+
 for s in strings:
     if pattern.match(s):
         print(s)
